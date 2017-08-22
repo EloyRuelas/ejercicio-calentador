bd7375a1f007b3ea68252abf9aebf0043ce361b5
Eloy Ruelas <eloy.rue@hotmail.com>
2017-08-22 14:45:12 -0500

    Intercambio inicial de proyecto

---
+++ Calentador.java
@@ -0,0 +1,25 @@
+
+public class Calentador
+{
+    private int temperatura;
+    public Calentador()
+    {
+        temperatura=15;
+    }
+    
+    public void calentar()
+    {
+        temperatura=temperatura+5;
+    }
+    
+    public void enfriar()
+    {
+        temperatura=temperatura-5;
+        
+    }
+    
+    public int checarTemperatura()
+    {
+        return temperatura;
+    }
+}
